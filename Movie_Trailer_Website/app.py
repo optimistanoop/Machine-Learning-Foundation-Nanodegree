b76da60401545f7502fd9be7fb588e880965e235
optimistanoop <optimistanoop@gmail.com>
2017-06-22 10:00:43 +0530

    P-1 Movie Trailer Website

--- Movie_Trailer_Website/app.py
+++ Movie_Trailer_Website/app.py
@@ -8,12 +8,12 @@ school_of_rock = media.Movie(
     "School of Rock",
     "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
     "https://www.youtube.com/watch?v=XCwy6lW5Ixc")
-    
+
 avatar = media.Movie(
     "Avatar",
     "http://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
     "https://www.youtube.com/watch?v=d1_JBMrrYw8")
-    
+
 midnight_in_paris = media.Movie(
     "Midnight in Paris",
     "http://tinyurl.com/7zebr4h",
@@ -22,8 +22,8 @@ midnight_in_paris = media.Movie(
 hunger_games = media.Movie(
     "Hunger Games",
     "http://tinyurl.com/79m8ppm",
-    "https://www.youtube.com/watch?v=mfmrPu43DF8")        
-    
+    "https://www.youtube.com/watch?v=mfmrPu43DF8")
+
 toystory = media.Movie(
     "Toy Story",
     "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
@@ -34,9 +34,8 @@ ratatouille = media.Movie(
     "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
     "https://www.youtube.com/watch?v=c3sBBRxDAqk")
 
-
-movies = [school_of_rock, avatar, midnight_in_paris, hunger_games, toystory, ratatouille]
-
+movies = [school_of_rock,
+          avatar, midnight_in_paris, hunger_games, toystory, ratatouille]
 
 # Call open_movies_page to open trailer page
 
